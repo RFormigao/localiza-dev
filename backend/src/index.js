de1e97fd0f14176dadb520fe38b02ca8a3776ece
RFormigao <rformigao.gomes@gmail.com>
2020-01-17 00:30:53 -0300

    First commit

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://robinho:knop01234@cluster0-ukav7.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB (Não-relacional)
+
+
+
+app.listen(3333)
