7169284a8480c44864af209cb8ff50ffe81e727d
RFormigao <rformigao.gomes@gmail.com>
2020-01-19 21:28:56 -0300

    finish mobile project 1.0.0

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native'
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7d40e7" />
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontWeight: 'bold', 
-    fontSize: 32,
-    color: '#fff',
-  }
-});
